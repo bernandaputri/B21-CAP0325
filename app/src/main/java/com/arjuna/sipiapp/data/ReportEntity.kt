392214de09b701a9ca0dfb7c6bea302f388e4d71
bernandaputri <haihaiputri@gmail.com>
2021-06-07 23:06:28 +0700

    move ui

---
+++ app/src/main/java/com/arjuna/sipiapp/data/ReportEntity.kt
@@ -0,0 +1,8 @@
+package com.arjuna.sipiapp.data
+
+data class ReportEntity(
+    var reportName: String?,
+    var reportLocation: String?,
+    var reportImage: String?,
+    var reportResult: String?
+)
---
+++ app/src/main/java/com/arjuna/sipiapp/ui/report/ResultReportFragment.kt
@@ -0,0 +1,20 @@
+package com.arjuna.sipiapp.ui.report
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arjuna.sipiapp.R
+
+class ResultReportFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result_report, container, false)
+    }
+
+}
