d6f9162ce1fdb434bbe0e28c57027ca58d0a76ad
bernandaputri <haihaiputri@gmail.com>
2021-06-07 21:32:30 +0700

    move ui

--- app/src/main/java/com/arjuna/sipiapp/MainActivity.kt
+++ app/src/main/java/com/arjuna/sipiapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.arjuna.sipiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_add_report, R.id.navigation_profile))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/arjuna/sipiapp/ui/addreport/AddReportFragment.kt
@@ -0,0 +1,20 @@
+package com.arjuna.sipiapp.ui.addreport
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arjuna.sipiapp.R
+
+class AddReportFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_report, container, false)
+    }
+
+}
